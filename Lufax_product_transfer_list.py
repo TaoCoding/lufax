a0f31f6af0a45c8f0b99d508ee447d51b98b13ec
unknown <jingwei@MSH-PC2987.marvell.com>
2014-10-15 10:31:03 +0800

    add time info in log, and change the db structure

--- Lufax_product_transfer_list.py
+++ Lufax_product_transfer_list.py
@@ -14,25 +14,29 @@ import simplejson  # for translation of str and dict
 import sqlite3    # for database
 import os
 
-while True:
-    def str_current_time():
+def str_current_time():
         current_time=time.time()*1000
         return str(current_time)
+    
+def iso_current_time(): 
+    ISOTIMEFORMAT='%Y-%m-%d %X'         #time format
+    current_time=time.strftime(ISOTIMEFORMAT,time.localtime())   #current time
+    return current_time
+    
+#format the souce link, then get the html, tranlate to dict
+def get_html(page_number):  
+    pre_link='https://list.lufax.com/list/service/product/transfer-product-list/listing/'
+    page_number=str(page_number)
+    suffix_link='?minAmount=0&maxAmount=100000000&column=currentPrice&tradingMode=00&order=asc&isDefault=false&_='
+    #trading mode=00 只显示一口价，不显示竞拍, column=currentPrice,控制排序，否则得话会有重复
+    source=pre_link+page_number+suffix_link+str_current_time()
+    html=urllib.urlopen(source).read()   #the got data is a string,
+    html=simplejson.loads(html)         # translate to dict
+    return html
 
 
-    #format the souce link, then get the html, tranlate to dict
-    def get_html(page_number):  
-        pre_link='https://list.lufax.com/list/service/product/transfer-product-list/listing/'
-        page_number=str(page_number)
-        suffix_link='?minAmount=0&maxAmount=100000000&column=currentPrice&tradingMode=00&order=asc&isDefault=false&_='
-        #trading mode=00 只显示一口价，不显示竞拍, column=currentPrice,控制排序，否则得话会有重复
-        source=pre_link+page_number+suffix_link+str_current_time()
-        html=urllib.urlopen(source).read()   #the got data is a string,
-        html=simplejson.loads(html)         # translate to dict
-        return html
-
-
-
+while True:
+    print iso_current_time(),'start to capture'
     totalPage=get_html(1)['totalPage']  #get the total page
     product_sum=[]
     page_number=1
@@ -45,24 +49,25 @@ while True:
 
 
     #store the data in sqlite3 db
-    os.chdir('result')
-    conn=sqlite3.connect("lufax.db") #if not exist, will create
+    
+    filepath=os.getcwd()+"\\result\\lufax.db"
+    conn=sqlite3.connect(filepath) #if not exist, will create
     cursor=conn.cursor()
     #
     cursor.execute("""create table if not exists product_transfer_list(
-    productId varchar(20) primary key,
+    productId int primary key,
     link varchar(128),
-    productCode varchar(32),
+    productCode int,
     publishedDate varchar(128),
     doneDate varchar(128),
-    principal varchar(16),
-    reduceDays float(8)
+    principal float,
+    reduceDays float
     )""")
     i=1
     for product_detail in product_sum:
         
         
-        productId_s=str(product_detail['productId'])
+        productId_s=product_detail['productId']
         productCode_s=product_detail['code']
         link='https://list.lufax.com/list/productDetail?productId='+str(productId_s)
         publishedDate_s=product_detail['publishAtCompleteTime']
@@ -87,16 +92,18 @@ while True:
         if productId_s in productId_list:
             sql="update product_transfer_list set productCode=(?),link=(?),publishedDate=(?),doneDate=(?),principal=(?),reduceDays=(?) where productId=(?)"
             cursor.execute(sql,(productCode_s,link,publishedDate_s,doneDate_s,principal_s,reduceDays_s,productId_s,))
-            print 'productId exist, updating %d row'%i
+            print iso_current_time(),'productId exist, updating %d row'%i
         else:
             sql="insert into product_transfer_list(productId,productCode,link,publishedDate,doneDate,principal,reduceDays) values((?),(?),(?),(?),(?),(?),(?))"
             cursor.execute(sql,(productId_s,productCode_s,link,publishedDate_s,doneDate_s,principal_s,reduceDays_s))
-            print 'Inserting %d row' %i
+            print iso_current_time(),'Inserting %d row' %i
         i=i+1
          
     cursor.close()
     conn.commit()
     conn.close()
+    print iso_current_time(),'please wait 5 minutes'
+
     time.sleep(300)
 
 
